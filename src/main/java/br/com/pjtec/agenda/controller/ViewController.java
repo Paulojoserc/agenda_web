0e99bb5948714784a7ff11abad3df7b3d2436b3a
Paulojoserc <pjrc83@gmail.com>
2023-07-22 08:21:46 -0300

    Criação do projeto

---
+++ src/main/java/br/com/pjtec/agenda/controller/ViewController.java
@@ -0,0 +1,22 @@
+package br.com.pjtec.agenda.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ViewController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+	@RequestMapping("/cadastro")
+	public String cadastro() {
+		return "cadastro";
+	}
+	
+	@RequestMapping("/contato")
+	public String contato() {
+		return "contato";
+	}
+}
